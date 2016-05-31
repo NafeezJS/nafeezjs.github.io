7888c342adb1c6a503c8720cb4d5bf10ee40a93a
NafeezJS <info@nafeezjs.com>
2016-05-31 22:11:05 +0000

    updated routes folders

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-var app = angular.module('portfolio', [
-  'ngRoute', 'ngMap', 'ngAnimate'
-]);
+var app = angular.module('portfolio', ['ngRoute', 'ngMap', 'ngAnimate']);
 
-/**
-* Configure the Routes
-*/
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-    // Home
     .when("/", { templateUrl: "partials/home.html", controller: "PageCtrl" })
-    // Pages
     .when("/portfolio", { templateUrl: "partials/portfolio.html", controller: "PageCtrl" })
     .when("/resume", { templateUrl: "partials/resume.html", controller: "PageCtrl" })
     .when("/contact", { templateUrl: "partials/contact.html", controller: "PageCtrl" })
@@ -24,13 +17,13 @@ app.controller('PageCtrl', function ( $scope/*, $location, $http */) {
 
 app.controller("dataImagesWork", function ($scope) {
     $scope.images_work = [
-          { num: 1, category: 'frontend', src: "rgb/rgb1.PNG", description: 'Color picker game', url_details: "rgb_details.html" },
-          { num: 2, category: 'frontend', src: "keyboardsounds/1.png", description: 'Animation and sound events', url_details: "keyboardsounds_details.html" },
-          { num: 3, category: 'frontend', src: "thingstodo/thingstodo1.PNG", description: 'To-do list in jQuery', url_details: "thingstodo_details.html" },
-          { num: 4, category: 'frontend', src: "portfolio/1.PNG", description: 'Portfolio in AngularJS', url_details: "portfolio_details.html" },
-          { num: 5, category: 'frontend', src: "moviesales/moviesales1.PNG", description: 'Book movie tickets', url_details: "moviesales_details.html" },
-          { num: 6, category: 'fullstack', src: "readinglist/readinglist1.PNG", description: 'Reading list web app', url_details: "readinglist_details.html" },
-          { num: 7, category: 'fullstack', src: "cabinhub/5.PNG", description: 'Cabin rentals rating site', url_details: "cabinhub_details.html" }];
+          { num: 1, category: 'frontend', src: "rgb/rgb1.PNG", description: 'Color picker game', url_details: "details/rgb_details.html" },
+          { num: 2, category: 'frontend', src: "keyboardsounds/1.png", description: 'Animation and sound events', url_details: "details/keyboardsounds_details.html" },
+          { num: 3, category: 'frontend', src: "thingstodo/thingstodo1.PNG", description: 'To-do list in jQuery', url_details: "details/thingstodo_details.html" },
+          { num: 4, category: 'frontend', src: "portfolio/1.PNG", description: 'Portfolio in AngularJS', url_details: "details/portfolio_details.html" },
+          { num: 5, category: 'frontend', src: "moviesales/moviesales1.PNG", description: 'Book movie tickets', url_details: "details/moviesales_details.html" },
+          { num: 6, category: 'fullstack', src: "readinglist/readinglist1.PNG", description: 'Reading list web app', url_details: "details/readinglist_details.html" },
+          { num: 7, category: 'fullstack', src: "cabinhub/5.PNG", description: 'Cabin rentals rating site', url_details: "details/cabinhub_details.html" }];
 });
 
 
